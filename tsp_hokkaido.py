c30d78d0abf6d191024737931feacfc0085b41de
sisisa <105726977+sisisa@users.noreply.github.com>
2023-12-23 09:34:24 +0900

    Update tsp_hokkaido.py

--- tsp_hokkaido.py
+++ tsp_hokkaido.py
@@ -1,19 +1,25 @@
 import csv
 import numpy as np
 from scipy.spatial import distance
-from ortoolpy import tsp# GoogleのOR-Toolsを使用
+from ortoolpy import tsp
 
-NewChitose_Airport = [141.6811815, 42.7876057]
+# 新千歳空港の経度と緯度を指定
+NewChitose_Airport = [141.6884719, 42.78066411]
 
-# 開始地点を新千歳空港に指定
+# 新千歳空港を開始地点に指定
 start_point = NewChitose_Airport
 nodes = [start_point]
 
-with open('tsp_Hokkaido.csv') as f:
+with open('tsp_data.csv', encoding="Shift-JIS") as f:
     reader = csv.reader(f)
     header = next(f)
+    
     for row in reader:
-        nodes.append([ row[2], row[1] ])
+        nodes.append([ row[2], row[1] ])# Bの2列目から開始
 
+# データを浮動小数点数に変換する
+nodes = np.array(nodes).astype(float)
 dist = distance.cdist(nodes, nodes)
+
+# 巡回セールスマン問題を計算
 print(tsp(nodes, dist))
